8afb6cdb7d3ed42476bc0db3ec094bcbb7bbb274
Nati <121944905+NetanelZeltserman@users.noreply.github.com>
2024-10-09 17:12:48 +0000

    chore: remove hello_world endpoint that was used for testing

--- meme_gen/app/urls.py
+++ meme_gen/app/urls.py
@@ -1,13 +1,11 @@
-from app.views.hello_world import HelloWorldView
-from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+from django.urls import path
 
 urlpatterns = [
-    path('hello_world/', HelloWorldView.as_view(), name='hello_world'),
     path('login/', TokenObtainPairView.as_view(), name='token_login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
