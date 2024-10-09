8b7dfd87688d57cc9d60ca516c6456c64942c3de
Nati <121944905+NetanelZeltserman@users.noreply.github.com>
2024-10-09 13:21:42 +0000

    feat: add token authentication

--- meme_gen/app/urls.py
+++ meme_gen/app/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
 from app.views.hello_world import HelloWorldView
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
     path('hello_world/', HelloWorldView.as_view(), name='hello_world'),
+    path('login/', TokenObtainPairView.as_view(), name='token_login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
