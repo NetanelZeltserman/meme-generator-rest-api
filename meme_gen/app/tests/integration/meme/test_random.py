706217a97172f41a5a3e466e891d74b9b2fdaf36
Nati <121944905+NetanelZeltserman@users.noreply.github.com>
2024-10-13 02:32:21 +0000

    fix (tests): remove unnecessary manual user, template, and meme creation - use test util functions instead to reduce redundency

--- meme_gen/app/tests/integration/meme/test_random.py
+++ meme_gen/app/tests/integration/meme/test_random.py
@@ -1,30 +1,17 @@
-from app.tests.utils import create_meme, create_memes
-from app.models import Meme, MemeTemplate, User
+from app.tests.utils import create_meme_template, create_memes, create_test_user
 from app.serializers.meme import MemeSerializer
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
+from app.models import Meme
 
 class MemeRandomTestCase(APITestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username='user', password='pass')
-        self.template = MemeTemplate.objects.create(
-            name="Test Template",
-            image_url="http://example.com/template.jpg",
-            default_top_text="Default Top",
-            default_bottom_text="Default Bottom"
-        )
+        self.user = create_test_user()
+        self.template = create_meme_template()
         self.memes = create_memes(5, self.template, self.user)
         self.url_name = 'meme_random'
         self.url = reverse(self.url_name)
-
-    def create_meme(self, top_text='Test Top', bottom_text='Test Bottom'):
-        return Meme.objects.create(
-            template=self.template,
-            top_text=top_text,
-            bottom_text=bottom_text,
-            created_by=self.user
-        )
 
     def test_retrieve_existing_meme(self):
         self.client.force_authenticate(user=self.user)
